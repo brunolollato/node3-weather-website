8d62140e4c3a77767a042691879e07d16d508990
Bruno Lollato <lollato7@gmail.com>
2019-08-03 01:12:47 -0300

    add humidity to the forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,7 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location', undefined)
         } else {
-            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degrees out. There is a ' + body.currently.precipProbability + ' chance of rain')    
+            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degrees out and the humidity is ' + body.currently.humidity*100 + '%. There is a ' + body.currently.precipProbability + ' chance of rain')    
         }
     })
 }
